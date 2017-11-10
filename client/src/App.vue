2908f7de912c8f03557ee6922c7659d5ef21a336
rojimark <rmbedana@Roggies-MacBook-Pro.local>
2017-11-11 03:58:25 +0800

    setting up the basic app

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      products: [
+        {
+          name: 'Banana skin,',
+          price: 20
+        },
+        {
+          name: 'Shiny Star,',
+          price: 40
+        },
+        {
+          name: 'Green Shells,',
+          price: 60
+        },
+        {
+          name: 'Red Shells,',
+          price: 80
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
